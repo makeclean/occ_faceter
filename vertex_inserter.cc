7743531e12ab09cf719366cc62add045ca27ce29
Andrew Davis <andrew.davis@ukaea.uk>
2018-07-19 21:03:55 +0100

    Addded rtree and vertex inserter to help address the vertex insertion check in the faceter

---
+++ vertex_inserter.cc
@@ -0,0 +1,86 @@
+#include "vertex_inserter.hh"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  VertexInserter *vi = new VertexInserter(1.e-4);
+  moab::EntityHandle eh = 0;
+  moab::ErrorCode rval = moab::MB_FAILURE;
+  rval = vi->insert_vertex({0,0,0},eh);
+  rval = vi->insert_vertex({0,1,0},eh);
+  rval = vi->insert_vertex({0,0,1},eh);
+  rval = vi->insert_vertex({0,0,0},eh);
+  rval = vi->insert_vertex({0,1,0},eh);
+  rval = vi->insert_vertex({0,0,1},eh);
+  rval = vi->insert_vertex({0,0,1.-1.e-5},eh);
+  delete vi;
+  return 0;
+}
+
+bool callback(int id) {  
+  hits.push_back(id);
+  return true;
+}
+
+// constructor
+VertexInserter::VertexInserter(double tolerance) {
+  box_tolerance = tolerance;
+  count = 0;
+}
+
+// desctructor
+VertexInserter::~VertexInserter() {
+  boxes.clear();
+  rtree.RemoveAll();
+}
+
+// insert the given vertex into the moab database
+moab::ErrorCode VertexInserter::insert_vertex(std::array<double,3> point,
+					      moab::EntityHandle &handle) {
+  // find any vertices that are 'close' to the point we
+  // are trying to insert
+  // make a box and keep it 
+  Box search = Box(point,box_tolerance);
+
+  // search the tree
+  moab::EntityHandle eh = 0;
+  moab::ErrorCode rval = search_tree(search,eh);
+  if ( rval == moab::MB_ENTITY_NOT_FOUND ) { // vertex doesnt exist
+    // make new vertex
+    rtree.Insert(search.min.data(),search.max.data(),count);
+    // set the eh
+    eh = count;
+    search.setHandle(eh);
+    count++;
+    // keep it for later
+    boxes.push_back(search);
+  } else if (rval == moab::MB_SUCCESS) { // vertex already exists
+    handle = eh;
+  } 
+  return rval;
+}
+
+// search the tree for the box
+moab::ErrorCode VertexInserter::search_tree(const Box search,
+					    moab::EntityHandle &hit) {
+  // 
+  moab::EntityHandle rval = moab::MB_FAILURE;
+  int nhits = 0;
+  // search the tree
+  nhits = rtree.Search(search.min.data(),search.max.data(),callback);
+  
+  if ( !nhits ) {
+    std::cout << "no hit found" << std::endl;
+    return moab::MB_ENTITY_NOT_FOUND;
+   } else {
+    // check the near hits for exactness
+    std::cout << nhits << " hits!" << std::endl;
+    for ( int  i = 0 ; i < nhits ; i++ ) {
+      moab::EntityHandle eh = boxes[hits[i]].getHandle();
+      std::cout << "hit box " << hits[i] << " with handle " << eh << std::endl;
+    }
+    hits.clear();
+    return moab::MB_SUCCESS;
+  }
+  return moab::MB_FAILURE;
+}
+
---
+++ vertex_inserter.hh
@@ -0,0 +1,113 @@
+#include "rtree/RTree.h"
+#include "moab/Types.hpp"
+#include <iostream>
+
+// global vector of hits
+std::vector<int> hits;
+
+// class to handle the insertion of vertices into moab such
+// that they are garenteed to be unique. Each point to be added
+// is searched in the tree within the specified tolerance and
+// any valid hits are are stored. Each hit is compared for exactness
+// if any hit returned is valid it is returned, otherwise the
+// vertex is added to the database
+
+// Box struct to assist in the creation of the rtree. Boxes
+// store which moab entity handle they surround 
+//
+struct Box {
+  // empty constuctor
+  Box();
+  Box(std::array<double,3> point,
+      double tolerance,
+      moab::EntityHandle handle = 0) {
+    
+    centre = point;
+    min = { point.data()[0] - tolerance,
+	    point.data()[1] - tolerance,
+	    point.data()[2] - tolerance};
+    max = { point.data()[0] + tolerance,
+	    point.data()[1] + tolerance,
+	    point.data()[2] + tolerance};
+    this->handle = handle;
+   }
+  
+  // array based constructor
+  Box(std::array<double,3> centre,
+      std::array<double,3> min,
+      std::array<double,3> max,
+      moab::EntityHandle handle = 0) {
+    this->centre = centre;
+    this->min = min;
+    this->max = max;
+    this->handle = handle;
+  }
+  // simple constructor - calls array version
+  Box(double x_0, double y_0, double z_0,
+      double x_min, double y_min, double z_min,
+      double x_max, double y_max, double z_max,
+      moab::EntityHandle handle = 0) {
+    std::array<double,3> centre = {x_0,y_0,z_0};
+    std::array<double,3> min = {x_min,y_min,z_min};
+    std::array<double,3> max = {x_max,y_max,z_max};
+    Box(centre,min,max,handle);
+  }
+  
+  // set the handle that this box points to
+  void setHandle(moab::EntityHandle handle) {
+    this->handle = handle;
+  }
+
+  // get the handle this box points to
+  moab::EntityHandle getHandle() {
+    return handle;
+  }
+
+  // print the contents of the box
+  void print() {
+    std::cout << centre.data()[0] << " ";
+    std::cout << centre.data()[1] << " ";
+    std::cout << centre.data()[2] << std::endl;
+    std::cout << min.data()[0] << " ";
+    std::cout << min.data()[1] << " ";
+    std::cout << min.data()[2] << std::endl;
+    std::cout << max.data()[0] << " ";
+    std::cout << max.data()[1] << " ";
+    std::cout << max.data()[2] << std::endl;
+    return;
+  }
+  
+  public:
+    std::array<double,3> centre; /// the midpoint of the box
+    std::array<double,3> min;    /// lowest coordinates of the box
+    std::array<double,3> max;    /// largest coorinates of the box
+  private:
+    moab::EntityHandle handle;   /// entity handle of the vertex  
+};
+
+class VertexInserter {
+  public:
+    VertexInserter(double tolerance = 1.0e-6);
+   ~VertexInserter();
+
+    // insert a vertex into the tree, if rval moab::entity_not_found
+    // its a new unique vertex - else moab::entity_found handle points
+    // to the existing vertex
+    moab::ErrorCode insert_vertex(std::array<double,3> point,
+				  moab::EntityHandle &handle);
+  
+  private:
+    // search the rtree for any hits with the specified box
+    // returns moab::MB_SUCCESS for found an exisiting entry
+    // returns moab::MB_ENTITY_NOT_FOUND for a new entry
+    // returns moab::MB_FAILURE for everthing else
+    moab::ErrorCode search_tree(const Box search_box,
+				moab::EntityHandle &hit);
+  
+  private:
+    RTree<int,double,3,float> rtree; /// the rtree to check against 
+    double box_tolerance; /// the insertion search tolerance
+    int count; /// the number of boxes in the rtree
+    std::vector<Box> boxes; // vector of boxes added
+};
+

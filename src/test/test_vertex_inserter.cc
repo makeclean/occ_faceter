b0e6ac0b44c937566925faa687d02357902ffaf6
Andrew Davis <andrew.davis@ukaea.uk>
2018-07-23 08:31:23 +0100

    Added more cases to test - confirms tolerance

--- src/test/test_vertex_inserter.cc
+++ src/test/test_vertex_inserter.cc
@@ -13,13 +13,26 @@ TEST_CASE("test insertion", "[insert_vertex]") {
   REQUIRE(rval == moab::MB_ENTITY_NOT_FOUND);
   rval = vi->insert_vertex({0,0,1},eh);
   REQUIRE(rval == moab::MB_ENTITY_NOT_FOUND);
-  rval = vi->insert_vertex({0,0,0},eh);
-  REQUIRE(rval == moab::MB_SUCCESS);
+  rval = vi->insert_vertex({0,0,0},eh); 
+  REQUIRE(rval == moab::MB_SUCCESS); // entity already exists
   rval = vi->insert_vertex({0,1,0},eh);
-  REQUIRE(rval == moab::MB_SUCCESS);
+  REQUIRE(rval == moab::MB_SUCCESS); // entity already exists
   rval = vi->insert_vertex({0,0,1},eh);
   REQUIRE(rval == moab::MB_SUCCESS);
-  rval = vi->insert_vertex({0,0,1.-1.e-5},eh);
+  rval = vi->insert_vertex({0,0,1.-1.e-1},eh); // entity 
+  REQUIRE(rval == moab::MB_ENTITY_NOT_FOUND); // entity already exists
+  rval = vi->insert_vertex({0,0,1.-1.e-2},eh); // entity 
+  REQUIRE(rval == moab::MB_ENTITY_NOT_FOUND); // entity already exists
+  rval = vi->insert_vertex({0,0,1.-1.e-3},eh); // entity 
+  REQUIRE(rval == moab::MB_ENTITY_NOT_FOUND); // entity already exists
+  rval = vi->insert_vertex({0,0,1.-1.e-4},eh); // entity 
+  REQUIRE(rval == moab::MB_ENTITY_NOT_FOUND); // entity already exists
+  rval = vi->insert_vertex({0,0,1.-1.e-5},eh); // entity 
+  REQUIRE(rval == moab::MB_ENTITY_NOT_FOUND); // entity already exists
+  rval = vi->insert_vertex({0,0,1.-1.e-6},eh); // entity 
+  REQUIRE(rval == moab::MB_SUCCESS); // entity already exists
+  rval = vi->insert_vertex({0,0,1.-1.e-7},eh); // entity 
+  REQUIRE(rval == moab::MB_SUCCESS); // entity doesnt exist
   delete vi;
 }
 

3a2278248f9ba7a1063fc3d6d2478d72e5b4f15c
Andrew Davis <andrew.davis@ukaea.uk>
2018-08-10 15:16:18 +0000

    finally fix build issues

--- cmake/FindOCE.cmake
+++ cmake/FindOCE.cmake
@@ -1,12 +1,18 @@
 find_path(OCE_CMAKE_CONFIG NAMES OCEConfig.cmake 
   HINTS ${OCE_ROOT}
-  PATHS ENV LD_LIBRARY_PATH DYLD_LIBRARY_PATH
+  PATHS ${OCE_ROOT} ENV LD_LIBRARY_PATH DYLD_LIBRARY_PATH
   # ubuntu path
-  PATHS /usr/lib/x86_64-linux-gnu/oce-0.17
   PATH_SUFFIXES lib Lib cmake cmake/OCE cmake/opencascade
   NO_DEFAULT_PATH)
 
-MESSAGE(status ${OCE_CMAKE_CONFIG})
+find_path(OCE_CMAKE_CONFIG NAMES OCEConfig.cmake 
+  ENV LD_LIBRARY_PATH DYLD_LIBRARY_PATH
+  # ubuntu path
+  PATHS ${HOME}/opt/oce/include/oce-0.19-dev/
+#  PATHS /usr/lib/x86_64-linux-gnu/oce-0.17
+  PATH_SUFFIXES lib Lib cmake cmake/OCE cmake/opencascade
+  NO_DEFAULT_PATH)
+
 if(OCE_CMAKE_CONFIG STREQUAL "OCE_CMAKE_CONFIG-NOTFOUND")
   set(OCE_FOUND FALSE)
 else()

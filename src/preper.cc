3483dd7f3731c5b04ce6dd3bee739c0ca88a3317
Andrew Davis <andrew.davis@ukaea.uk>
2018-09-27 14:13:08 +0000

    Almost wokring

--- src/preper.cc
+++ src/preper.cc
@@ -9,16 +9,24 @@
 #include "CGAL/AABB_tree.h"
 #include "CGAL/AABB_traits.h"
 #include "CGAL/Polygon_mesh_slicer.h"
-#include "CGAL/Polygon_mesh_processing/corefinement.h"
+//#include "CGAL/Polygon_mesh_processing/corefinement.h"
 #include "CGAL/Point_3.h"
 #include "CGAL/predicates_on_points_3.h"
+#include "CGAL/Polyhedron_3.h"
+#include "CGAL/Polyhedron_incremental_builder_3.h"
+#include "CGAL/Nef_polyhedron_3.h"
+#include <CGAL/Polyhedron_items_with_id_3.h> 
+
+
+
+#include "vertex_inserter.hh"
 
 // system includes
 #include <fstream>
 #include <iostream>
 
 // cgal typedefs
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Exact_predicates_exact_constructions_kernel K;
 typedef CGAL::Surface_mesh<K::Point_3> Mesh;
 typedef std::vector<K::Point_3> Polyline_type;
 typedef std::list< Polyline_type > Polylines;
@@ -27,7 +35,161 @@ typedef CGAL::AABB_traits<K, HGSP>    AABB_traits;
 typedef CGAL::AABB_tree<AABB_traits>  AABB_tree;
 typedef K::Point_3 Vertex;
 
+//typedef CGAL::Polyhedron_3<K> Polyhedron;
+typedef CGAL::Polyhedron_3<K,CGAL::Polyhedron_items_with_id_3>  Polyhedron;
+//typedef CGAL::Polyhedron_items_with_id_3<K> Polyhedron; 
+typedef Polyhedron::HalfedgeDS HalfedgeDS;
+typedef CGAL::Nef_polyhedron_3<K> Nef_Polyhedron;
+//typedef Polyhedron::Vertex Vertex;
+//typedef Polyhedron::Vertex_iterator Vertex_iterator;
+//typedef Polyhedron::Facet_iterator Facet_iterator;
+
 moab::Core *MBI = NULL;
+
+const char geom_categories[][CATEGORY_TAG_SIZE] = {"Vertex\0",
+						   "Curve\0",
+						   "Surface\0",
+						   "Volume\0",
+						   "Group\0"};
+
+// class to store the new volume information
+class NewVolsMOAB {
+  public:
+  // constructor
+  NewVolsMOAB(){
+    MOAB = new moab::Core();
+    vi = new VertexInserter(MOAB);
+  }
+
+  void setup_tags() {
+    moab::ErrorCode rval = moab::MB_FAILURE;
+    rval = MOAB->tag_get_handle(GEOM_DIMENSION_TAG_NAME, 1, moab::MB_TYPE_INTEGER, geometry_dimension_tag, moab::MB_TAG_DENSE | moab::MB_TAG_CREAT);
+    MB_CHK_SET_ERR_RET(rval, "Couldnt get geom dim tag");
+
+    rval = MOAB->tag_get_handle(GLOBAL_ID_TAG_NAME, 1, moab::MB_TYPE_INTEGER, id_tag, moab::MB_TAG_DENSE | moab::MB_TAG_CREAT);
+    MB_CHK_SET_ERR_RET(rval, "Couldnt get id tag");
+    
+    rval = MOAB->tag_get_handle("FACETING_TOL", 1, moab::MB_TYPE_DOUBLE, faceting_tol_tag,
+		  	     moab::MB_TAG_SPARSE | moab::MB_TAG_CREAT);
+    MB_CHK_SET_ERR_RET(rval, "Error creating faceting_tol_tag");
+
+    rval = MOAB->tag_get_handle("GEOMETRY_RESABS", 1, moab::MB_TYPE_DOUBLE, 
+		  	     geometry_resabs_tag, moab::MB_TAG_SPARSE | moab::MB_TAG_CREAT);
+    MB_CHK_SET_ERR_RET(rval, "Error creating geometry_resabs_tag");
+    
+    rval = MOAB->tag_get_handle(CATEGORY_TAG_NAME, CATEGORY_TAG_SIZE, moab::MB_TYPE_OPAQUE, 
+	  		     category_tag, moab::MB_TAG_SPARSE | moab::MB_TAG_CREAT);
+    MB_CHK_SET_ERR_RET(rval, "Error creating category_tag");
+
+    rval = MOAB->tag_get_handle(NAME_TAG_NAME, NAME_TAG_SIZE, moab::MB_TYPE_OPAQUE,
+	  		     name_tag, moab::MB_TAG_SPARSE | moab::MB_TAG_CREAT);
+  
+    return;
+  }
+
+  // set the surface tags
+  void set_surface_tags(int id, moab::EntityHandle surface_set) {
+    const void* id_val = &id;
+    moab::ErrorCode rval;
+
+    int val_s = 2;
+    const void* val_surf = &val_s;
+    rval = MOAB->tag_set_data(category_tag,&surface_set,1,&geom_categories[2]);
+    rval = MOAB->tag_set_data(geometry_dimension_tag,&surface_set,1,val_surf);
+    rval = MOAB->tag_set_data(id_tag,&surface_set,1,id_val);
+    return;
+  }
+
+  // set the volume tags
+  void set_volume_tags(int id, moab::EntityHandle volume_set) {
+    const void* id_val = &id;
+    moab::ErrorCode rval;
+
+    int val_v = 3;
+    const void* val_vol = &val_v;
+    rval = MOAB->tag_set_data(category_tag,&volume_set,1,&geom_categories[3]);
+    rval = MOAB->tag_set_data(geometry_dimension_tag,&volume_set,1,val_vol);
+    rval = MOAB->tag_set_data(id_tag,&volume_set,1,id_val);
+    return;
+  }
+
+  // get trangles and verts from polyhedron
+  moab::ErrorCode getTrisFromPoly(Polyhedron p, moab::Range &triangles) {
+    std::cout << p.size_of_facets() << std::endl;
+    moab::ErrorCode rval;
+    if(p.size_of_facets() == 0 ) return moab::MB_FAILURE;
+    //
+    std::unordered_map<int,moab::EntityHandle> index_handle_map;
+    int idx = 0;
+    for ( Polyhedron::Vertex_iterator v = p.vertices_begin() ; 
+          v != p.vertices_end() ; ++v ) {
+        v->id() = idx++;
+        CGAL::Point_3<K> point = v->point();
+        std::array<double,3> coords(point.x(),point.y(),point.z());
+        moab::EntityHandle handle;
+        rval = vi->insert_vertex(coords,handle);
+        index_handle_map[v->id()] = handle;
+    } 
+
+    idx = 0;
+    // loop over the facets in the shape
+    for ( Polyhedron::Facet_iterator  f = p.facets_begin() ; 
+          f != p.facets_end() ; ++f ) {
+      f->id() = idx++;
+      Polyhedron::Halfedge_around_facet_circulator circ = f->facet_begin(); 
+      std::vector<moab::EntityHandle> connectivity;
+      do { 
+        int id = circ->vertex()->id(); 
+        moab::EntityHandle handle = index_handle_map[id];
+        connectivity.push_back(handle);
+      } while ( ++circ != f->facet_begin());
+      // make a new triangle
+      assert(connectivity.size() == 3);
+      moab::EntityHandle triangle;
+      rval = MOAB->create_element(moab::MBTRI,&connectivity[0],3,triangle); 
+      triangles.insert(triangle);
+    }
+
+    return moab::MB_SUCCESS;
+  }
+
+  // add polyedron with id id
+  moab::ErrorCode addPolyhedron(Polyhedron p, int id) {
+    moab::ErrorCode rval = moab::MB_FAILURE;
+    moab::Range surface;
+    rval = getTrisFromPoly(p,surface);
+    return rval;
+    // tag the surface set
+    moab::EntityHandle surface_set;
+    rval = MOAB->create_meshset(moab::MESHSET_SET,surface_set);
+    set_surface_tags(id,surface_set);
+ 
+    // tag the volume set
+    moab::EntityHandle volume_set;
+    rval = MOAB->create_meshset(moab::MESHSET_SET,volume_set);
+    set_volume_tags(id,volume_set);
+
+    // add parent child links
+    rval = MOAB->add_parent_child(volume_set,surface_set);
+    // add the triangles to the surface set 
+    rval = MOAB->add_entities(surface_set,surface); 
+
+  }
+
+  // destructor
+  ~NewVolsMOAB(){
+    delete MOAB;
+  }
+  moab::Core *MOAB;
+  VertexInserter *vi;
+  // tags
+  moab::Tag geometry_dimension_tag;
+  moab::Tag id_tag;
+  moab::Tag faceting_tol_tag;
+  moab::Tag geometry_resabs_tag;
+  moab::Tag category_tag;
+  moab::Tag name_tag;
+};
 
 class MOABInterface {
   public:
@@ -37,6 +199,60 @@ class MOABInterface {
   }
   ~MOABInterface(){
   }
+
+// A modifier creating a triangle with the incremental builder.
+template <class HDS>
+class Build_triangle : public CGAL::Modifier_base<HDS> {  
+  public:
+  Build_triangle(moab::Core *mbi,
+		 const moab::Range verts,
+		 const moab::Range tris) {
+    MBI = mbi;
+    vertices = verts;
+    triangles = tris;
+  }
+  
+    void operator()( HDS& hds) {
+      // Postcondition: hds is a valid polyhedral surface.
+
+      CGAL::Polyhedron_incremental_builder_3<HDS> B( hds, true);
+      B.begin_surface( vertices.size(), triangles.size(),
+		       triangles.size()*3, 0 );
+      
+      typedef typename HDS::Vertex Vertex;
+      typedef typename Vertex::Point Point;
+
+      // add all the vertices
+      std::map<moab::EntityHandle,int> cgal_verts;
+      double pos[3];
+      int idx = 0;
+      moab::ErrorCode rval = moab::MB_FAILURE;
+      for ( moab::EntityHandle vertex : vertices) {
+	      rval = MBI->get_coords(&(vertex),1,&pos[0]);
+	      B.add_vertex(Point(pos[0],pos[1],pos[2]));
+	      cgal_verts[vertex] = idx;
+	      idx++;
+      }
+
+      // loop over the triangles and add them
+      //      B.begin_surface();
+      std::vector<moab::EntityHandle> verts;
+      for ( moab::EntityHandle triangle : triangles ) {
+	      B.begin_facet();
+	      rval = MBI->get_connectivity(&(triangle), 1, verts);
+	      B.add_vertex_to_facet( cgal_verts[verts[0]]);
+	      B.add_vertex_to_facet( cgal_verts[verts[1]]);
+	      B.add_vertex_to_facet( cgal_verts[verts[2]]);
+        B.end_facet();
+      }
+      B.end_surface();
+    }
+  
+  private: 
+    moab::Range triangles;
+    moab::Range vertices;
+    moab::Core *MBI;
+};
   
   // setup 
   void setupInterface() {
@@ -107,14 +323,7 @@ class MOABInterface {
       Mesh::Vertex_index point1 = cgal_verts[verts[0]];
       Mesh::Vertex_index point2 = cgal_verts[verts[1]];
       Mesh::Vertex_index point3 = cgal_verts[verts[2]];			     
-      /*
-      switch (CGAL::orientation(point1,point2,point3)) {
-        case CGAL::LEFTTURN: std::cout << "Left Turn\n"; break;
-        case CGAL::RIGHTURN: std::cout << "Right Turn\n"; break;
-        case CGAL::COLLINEAR: std::cout << "No Turn\n"; break;	
-      }
-      */
-      //mesh.add_face(point1,point2,point3);
+
       std::vector<Mesh::Vertex_index> points;
       points.push_back(point1);
       points.push_back(point2);
@@ -124,7 +333,40 @@ class MOABInterface {
     }
     return mesh;
   }
-  
+
+  // make a CGAL polyhedron object from a bag of triangles
+  Polyhedron makeCGALPolyhedron(moab::Range triangles) {
+    moab::Range vertices;
+    moab::ErrorCode rval = moab::MB_FAILURE;
+    rval = MBI->get_vertices(triangles, vertices);
+    // now get make an indexable list
+    moab::Range::iterator it;
+
+    Polyhedron p;
+    Build_triangle<HalfedgeDS> triangle(MBI,vertices,triangles);
+    p.delegate(triangle);
+    return p;
+    /*
+
+    // now for each triangle make the triangle
+    for ( it = triangles.begin() ; it != triangles.end() ; ++it) {
+      std::vector<moab::EntityHandle> verts;
+      rval = MBI->get_connectivity(&(*it),1, verts);
+      Mesh::Vertex_index point1 = cgal_verts[verts[0]];
+      Mesh::Vertex_index point2 = cgal_verts[verts[1]];
+      Mesh::Vertex_index point3 = cgal_verts[verts[2]];			     
+
+      std::vector<Mesh::Vertex_index> points;
+      points.push_back(point1);
+      points.push_back(point2);
+      points.push_back(point3);
+      
+      mesh.add_face(points);
+      }
+    */
+    return p;
+  }
+
   // for a given dagmc volume get a mesh
   Mesh getCGALMesh(int vol_id) {
     moab::ErrorCode rval;
@@ -132,6 +374,15 @@ class MOABInterface {
     Mesh mesh = makeCGALMesh(triangles);
     return mesh;
   }
+
+  // for a given dagmc volume get a mesh
+  Polyhedron getCGALMeshAsPolyhedron(int vol_id) {
+    moab::ErrorCode rval;
+    moab::Range triangles = getChildTriangles(vol_id);
+    Polyhedron p = makeCGALPolyhedron(triangles);
+    return p;
+  }
+
 
   // slice through the whole geometry
   std::map<int,Polylines> sliceGeometry(double dir[3], double offset) {
@@ -155,6 +406,42 @@ class MOABInterface {
     }
   }
 
+  // get the CGAL version of geometry
+  void makeCGALGeometryPoly() {
+   std::map<int, moab::EntityHandle>::iterator it;
+    for ( it = volmap.begin() ; it != volmap.end() ; ++it ) {
+      Polyhedron p = getCGALMeshAsPolyhedron(it->first);
+      geometry_poly[it->first] = p;
+    }   
+  }
+
+  // subtract vol1 from vol2
+  void subtract_volumes_poly(int vol1, int vol2){
+    Polyhedron p1 = geometry_poly[vol1];
+    Polyhedron p2 = geometry_poly[vol2];
+
+    Nef_Polyhedron np1(p1);
+    Nef_Polyhedron np2(p2);
+    
+    Nef_Polyhedron diff = np2 - np1;
+
+    np1.clear();
+    np2.clear();
+
+    Polyhedron difference;
+
+    diff.convert_to_polyhedron(difference);
+
+    diff.clear();
+
+    NewVolsMOAB *nvm = new NewVolsMOAB();
+    moab::ErrorCode rval = nvm->addPolyhedron(difference,1);
+ 
+
+    return;
+  }
+  
+  /*
   // given two volumes - subtract them
   void subtract_volumes() {
     std::cout << "Subtract volumes" << std::endl;
@@ -185,7 +472,9 @@ class MOABInterface {
     }
     return;
   }
+  */
 
+  /*
   // given two volumes - subtract them
   void subtract_volumes_mesh() {
     std::cout << "Subtract volumes" << std::endl;
@@ -234,11 +523,13 @@ class MOABInterface {
     }
     return;
   }
+  */
   
   private:
   moab::Tag geometry_dimension_tag, id_tag;
   std::map<int, moab::EntityHandle> volmap;
   std::map<int, Mesh> geometry;
+  std::map<int, Polyhedron> geometry_poly;
 };
 
 
@@ -255,8 +546,7 @@ int main(int argc, char* argv[]) {
   
   // make the cgal geometry
   MOABInterface *cgal = new MOABInterface();
-  cgal->makeCGALGeometry();
-  cgal->subtract_volumes_mesh();
-  
+  cgal->makeCGALGeometryPoly();
+  cgal->subtract_volumes_poly(1,2);
   return 0;
 }

1dba5d6a1a2843d2c4bd134e85f215974f3666f1
Andrew Davis <andrew.davis@ukaea.uk>
2019-10-29 07:57:02 +0000

    can now mesh simple geometries

--- src/dagmc_2d_mesh.cc
+++ src/dagmc_2d_mesh.cc
@@ -37,12 +37,21 @@ struct FaceInfo2 {
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K2;
 
-typedef CGAL::Triangulation_vertex_base_2<K2>                      Vb;
+/*
 typedef CGAL::Triangulation_face_base_with_info_2<FaceInfo2,K2>    Fbb;
 typedef CGAL::Constrained_triangulation_face_base_2<K2,Fbb>        Fb;
 typedef CGAL::Triangulation_data_structure_2<Vb,Fb>               TDS;
 typedef CGAL::Exact_predicates_tag                                Itag;
 typedef CGAL::Constrained_Delaunay_triangulation_2<K2, TDS, Itag>  CDT;
+typedef CGAL::Delaunay_mesh_size_criteria_2<CDT>            Criteria;
+typedef CGAL::Delaunay_mesher_2<CDT, Criteria>              Mesher;
+*/
+//typedef CGAL::Delaunay_mesh_vertex_base_2<K2>                Vb;
+
+typedef CGAL::Triangulation_vertex_base_2<K2>                      Vb;
+typedef CGAL::Delaunay_mesh_face_base_2<K2>                  Fb;
+typedef CGAL::Triangulation_data_structure_2<Vb, Fb>        Tds;
+typedef CGAL::Constrained_Delaunay_triangulation_2<K2, Tds>  CDT;
 typedef CGAL::Delaunay_mesh_size_criteria_2<CDT>            Criteria;
 typedef CGAL::Delaunay_mesher_2<CDT, Criteria>              Mesher;
 
@@ -57,53 +66,6 @@ typedef CDT::Point Point;
 typedef std::list<Polygon_2> Polygon_list;
 
 moab::Tag idtag;
-
-void mark_domains(CDT& ct, 
-                  CDT::Face_handle start, 
-                  int index, 
-                  std::list<CDT::Edge>& border ) {
-  if(start->info().nesting_level != -1){
-    return;
-  }
-  std::list<CDT::Face_handle> queue;
-  queue.push_back(start);
-  while(! queue.empty()){
-    CDT::Face_handle fh = queue.front();
-    queue.pop_front();
-    if(fh->info().nesting_level == -1){
-      fh->info().nesting_level = index;
-      for(int i = 0; i < 3; i++){
-        CDT::Edge e(fh,i);
-        CDT::Face_handle n = fh->neighbor(i);
-        if(n->info().nesting_level == -1){
-          if(ct.is_constrained(e)) border.push_back(e);
-          else queue.push_back(n);
-        }
-      }
-    }
-  }
-}
-//explore set of facets connected with non constrained edges,
-//and attribute to each such set a nesting level.
-//We start from facets incident to the infinite vertex, with a nesting
-//level of 0. Then we recursively consider the non-explored facets incident 
-//to constrained edges bounding the former set and increase the nesting level by 1.
-//Facets in the domain are those with an odd nesting level.
-void mark_domains(CDT& cdt) {
-  for(CDT::All_faces_iterator it = cdt.all_faces_begin(); it != cdt.all_faces_end(); ++it){
-    it->info().nesting_level = -1;
-  }
-  std::list<CDT::Edge> border;
-  mark_domains(cdt, cdt.infinite_face(), 0, border);
-  while(! border.empty()){
-    CDT::Edge e = border.front();
-    border.pop_front();
-    CDT::Face_handle n = e.first->neighbor(e.second);
-    if(n->info().nesting_level == -1){
-      mark_domains(cdt, n, e.first->info().nesting_level+1, border);
-    }
-  }
-}
 
 Polygon_2 make_poly_from_segments(const Polyline_type segments) {
 
@@ -204,130 +166,160 @@ void make_2d_triangulation_2(const std::map<int,Polylines> slices) {
     
     //
     Mesher mesher(cdt);
-    mesher.set_criteria(Criteria(0.125, 0.05));
+    mesher.set_criteria(Criteria(0.125, 1.0));
     mesher.refine_mesh();
-  }
-}
 
-// stride through the slices and make a triangulation
-void make_2d_triangulation(const std::map<int,Polylines> slices) {
-  // loop over the slices
-  //  std::vector<Polygon_2> polygons;
+    int count = 0;
 
-  moab::Core *MOAB = new moab::Core();
-  VertexInserter::VertexInserter *vi = new VertexInserter::VertexInserter(MOAB);
+    for ( CDT::Finite_faces_iterator tri = cdt.finite_faces_begin() ;
+	  tri != cdt.finite_faces_end() ;
+	  ++tri ) {
+      count++;
 
-  moab::ErrorCode rval = moab::MB_FAILURE;
-  rval = MOAB->tag_get_handle("ID_TAG", 1, moab::MB_TYPE_INTEGER, idtag, moab::MB_TAG_SPARSE | moab::MB_TAG_CREAT);
-  MB_CHK_SET_ERR_RET(rval, "Couldnt get geom dim tag");
-  
-  // loop over the slices
-  for ( std::pair<int,Polylines> boundary : slices ) {
-    std::cout << "id: " << boundary.first;// << std::endl;
-    std::cout << " size:" << boundary.second.size() << std::endl;
-   
-    int id = boundary.first;
-    //if ( id == 3 ) { break;}
-    //if ( id != 2 ) { continue;}
-    std::cout << std::scientific;
-    std::cout << std::setprecision(18);// << std::endl;
- 
-    CDT cdt;
-
-    // for each closed loop in the boundary 
-    for ( Polyline_type segments : boundary.second ) {
-      // for each point in a given loop
-      std::cout << "number of segments: " << segments.size() << std::endl;
-      // dont want only line segments
-      if ( segments.size() <= 1) continue;
-
-      Polygon_2 polygon;
-      segments.pop_back();
-      polygon = make_poly_from_segments(segments);
-
-      std::cout << "simple? " << polygon.is_simple() << " convex?" << polygon.is_convex() << std::endl;
-      if(!polygon.is_simple()) continue;
-      if(polygon.is_convex()) continue;
-
-      // if polygon not simple dont add it
-      // all non degenerate polys should be 
-      // simple
-
-      // due to the fact this this polygon could be 
-      // convex we need to partition and then 
-      // triangulate each partition
-      
-      Polygon_list poly_parts;
-      CGAL::Partition_traits_2<K2> partition_traits;
-      // generate partition for polygon
-      CGAL::approx_convex_partition_2(polygon.vertices_begin(),
-                                      polygon.vertices_end(),
-                                      std::back_inserter(poly_parts));
-      //std::cout << polygon << std::endl;
-      /*
-      CGAL::optimal_convex_partition_2(polygon.vertices_begin(),
-                                       polygon.vertices_end(),
-                                       std::back_inserter(poly_parts),
-                                       partition_traits);
-                                       */
-      std::cout << "partition: " << poly_parts.size() << std::endl;
-      //std::cout << polygon << std::endl;
-      // append the polygon to the vector
-      //polygons.push_back(polygon);    
-
-      for (Polygon_2 poly : poly_parts) {
-        // std::cout << poly << std::endl;
-        cdt.insert_constraint(poly.vertices_begin(),
-			                        poly.vertices_end(),
-                              true);
+      moab::EntityHandle triangle;
+      std::array<moab::EntityHandle,3> connectivity;
+      for ( int i = 0 ; i < 3 ; i++ ) {
+	double x = CGAL::to_double(tri->vertex(i)->point().hx());
+	double y = CGAL::to_double(tri->vertex(i)->point().hy());
+	std::cout << x << " " << y << std::endl;	  
+	std::array<double,3> coords = {x,y,0.0};
+	// make new moab vertex
+	rval = vi->insert_vertex(coords,connectivity[i]);
       }
 
-      mark_domains(cdt);
-
-      CDT::Finite_faces_iterator t; // = cdt.faces_begin();
-
-      // CGAL::draw(cdt);
-/*
-     for(CDT::Finite_faces_iterator fit = cdt.finite_faces_begin();
-      fit != cdt.finite_faces_end(); ++fit) {
-        if(fit->info().is_in_domain()) std::cout << "rah" << std::endl;
-      }
-*/
-      int count = 0;
-      for ( t = cdt.finite_faces_begin() ;
-           t != cdt.finite_faces_end() ;
-           ++t ) {
-
-        if(!(t->info().in_domain())) continue;
-        
-        count++;
-        moab::EntityHandle triangle;
-        std::array<moab::EntityHandle,3> connectivity;
-        for ( int i = 0 ; i < 3 ; i++ ) {
-
-          double x = CGAL::to_double(t->vertex(i)->point().hx());
-          double y = CGAL::to_double(t->vertex(i)->point().hy());
-
-	  std::cout << x << " " << y << std::endl;
-	  
-          std::array<double,3> coords = {x,y,0.0};
-          // make new moab vertex
-          rval = vi->insert_vertex(coords,connectivity[i]);
-        }
       // make a new triangle
       rval = MOAB->create_element(moab::MBTRI,&(connectivity[0]),3,triangle);
       // tag the triangle
       const void* vol_id = &id;
       rval = MOAB->tag_set_data(idtag,&triangle,1,&vol_id);
-      }
     }
-    //break;
-    std::cout << id << std::endl;
   }
   rval = MOAB->write_mesh("mesh.h5m");
   delete vi;
   delete MOAB;
 }
+
+// /*
+// // stride through the slices and make a triangulation
+// void make_2d_triangulation(const std::map<int,Polylines> slices) {
+//   // loop over the slices
+//   //  std::vector<Polygon_2> polygons;
+
+//   moab::Core *MOAB = new moab::Core();
+//   VertexInserter::VertexInserter *vi = new VertexInserter::VertexInserter(MOAB);
+
+//   moab::ErrorCode rval = moab::MB_FAILURE;
+//   rval = MOAB->tag_get_handle("ID_TAG", 1, moab::MB_TYPE_INTEGER, idtag, moab::MB_TAG_SPARSE | moab::MB_TAG_CREAT);
+//   MB_CHK_SET_ERR_RET(rval, "Couldnt get geom dim tag");
+  
+//   // loop over the slices
+//   for ( std::pair<int,Polylines> boundary : slices ) {
+//     std::cout << "id: " << boundary.first;// << std::endl;
+//     std::cout << " size:" << boundary.second.size() << std::endl;
+   
+//     int id = boundary.first;
+//     //if ( id == 3 ) { break;}
+//     //if ( id != 2 ) { continue;}
+//     std::cout << std::scientific;
+//     std::cout << std::setprecision(18);// << std::endl;
+ 
+//     CDT cdt;
+
+//     // for each closed loop in the boundary 
+//     for ( Polyline_type segments : boundary.second ) {
+//       // for each point in a given loop
+//       std::cout << "number of segments: " << segments.size() << std::endl;
+//       // dont want only line segments
+//       if ( segments.size() <= 1) continue;
+
+//       Polygon_2 polygon;
+//       segments.pop_back();
+//       polygon = make_poly_from_segments(segments);
+
+//       std::cout << "simple? " << polygon.is_simple() << " convex?" << polygon.is_convex() << std::endl;
+//       if(!polygon.is_simple()) continue;
+//       if(polygon.is_convex()) continue;
+
+//       // if polygon not simple dont add it
+//       // all non degenerate polys should be 
+//       // simple
+
+//       // due to the fact this this polygon could be 
+//       // convex we need to partition and then 
+//       // triangulate each partition
+      
+//       Polygon_list poly_parts;
+//       CGAL::Partition_traits_2<K2> partition_traits;
+//       // generate partition for polygon
+//       CGAL::approx_convex_partition_2(polygon.vertices_begin(),
+//                                       polygon.vertices_end(),
+//                                       std::back_inserter(poly_parts));
+//       //std::cout << polygon << std::endl;
+//       ///*
+//       //CGAL::optimal_convex_partition_2(polygon.vertices_begin(),
+//       //                                 polygon.vertices_end(),
+//       //                                 std::back_inserter(poly_parts),
+//       //                                 partition_traits);
+//       //                                 */
+//       std::cout << "partition: " << poly_parts.size() << std::endl;
+//       //std::cout << polygon << std::endl;
+//       // append the polygon to the vector
+//       //polygons.push_back(polygon);    
+
+//       for (Polygon_2 poly : poly_parts) {
+//         // std::cout << poly << std::endl;
+//         cdt.insert_constraint(poly.vertices_begin(),
+// 			                        poly.vertices_end(),
+//                               true);
+//       }
+
+//       mark_domains(cdt);
+
+//       CDT::Finite_faces_iterator t; // = cdt.faces_begin();
+
+//       // CGAL::draw(cdt);
+// ///*
+// //     for(CDT::Finite_faces_iterator fit = cdt.finite_faces_begin();
+// //      fit != cdt.finite_faces_end(); ++fit) {
+// //        if(fit->info().is_in_domain()) std::cout << "rah" << std::endl;
+// //     }
+// // */
+//       int count = 0;
+//       for ( t = cdt.finite_faces_begin() ;
+//            t != cdt.finite_faces_end() ;
+//            ++t ) {
+
+//         if(!(t->info().in_domain())) continue;
+        
+//         count++;
+//         moab::EntityHandle triangle;
+//         std::array<moab::EntityHandle,3> connectivity;
+//         for ( int i = 0 ; i < 3 ; i++ ) {
+
+//           double x = CGAL::to_double(t->vertex(i)->point().hx());
+//           double y = CGAL::to_double(t->vertex(i)->point().hy());
+
+// 	  std::cout << x << " " << y << std::endl;
+	  
+//           std::array<double,3> coords = {x,y,0.0};
+//           // make new moab vertex
+//           rval = vi->insert_vertex(coords,connectivity[i]);
+//         }
+//       // make a new triangle
+//       rval = MOAB->create_element(moab::MBTRI,&(connectivity[0]),3,triangle);
+//       // tag the triangle
+//       const void* vol_id = &id;
+//       rval = MOAB->tag_set_data(idtag,&triangle,1,&vol_id);
+//       }
+//     }
+//     //break;
+//     std::cout << id << std::endl;
+//   }
+//   rval = MOAB->write_mesh("mesh.h5m");
+//   delete vi;
+//   delete MOAB;
+// }
+// */
 
 int main(int argc, char* argv[]) {
 

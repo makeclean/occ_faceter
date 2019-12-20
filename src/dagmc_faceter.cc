10afd27f2e44320e4c300a20d344ca514a1bdf7f
John Nonweiler <john.nonweiler@ukaea.uk>
2019-12-20 16:02:09 +0000

    Load BRep instead of STEP file into occ_faceter

--- src/dagmc_faceter.cc
+++ src/dagmc_faceter.cc
@@ -151,15 +151,15 @@ void perform_faceting(TopoDS_Face face) {
   BRepMesh_IncrementalMesh facets(face, facet_tol, false, 0.5);
 }
 
-void facet_all_volumes(Handle_TopTools_HSequenceOfShape shape_list) {
-  int count = shape_list->Length();
+void facet_all_volumes(const TopTools_HSequenceOfShape& shape_list) {
+  int count = shape_list.Length();
 
   std::vector<TopoDS_Face> uniqueFaces;
   MapFaceToSurface surfaceMap;
 
   // list unique faces, create empty surfaces, and build surfaceMap
   for (int i = 1; i <= count; i++) {
-    TopoDS_Shape shape = shape_list->Value(i);
+    TopoDS_Shape shape = shape_list.Value(i);
     for (TopExp_Explorer ex(shape, TopAbs_FACE); ex.More(); ex.Next()) {
       TopoDS_Face face = TopoDS::Face(ex.Current());
       // Important note: For the surface map, face equivalence is defined
@@ -191,7 +191,7 @@ void facet_all_volumes(Handle_TopTools_HSequenceOfShape shape_list) {
 
   // create volumes and add surfaces
   for (int i = 1; i <= count; i++) {
-    TopoDS_Shape shape = shape_list->Value(i);
+    TopoDS_Shape shape = shape_list.Value(i);
 
     moab::EntityHandle vol;
     mbtool->make_new_volume(vol);
@@ -205,92 +205,47 @@ void facet_all_volumes(Handle_TopTools_HSequenceOfShape shape_list) {
   }
 }
 
-STEPControl_Reader *step;
-
-//dump all labels
-void dump_labels() {
-  const Handle(XSControl_WorkSession) &theSession = step->WS();
-  const Handle(Interface_InterfaceModel) &theModel = theSession->Model();
-
-  Standard_Integer nb = theModel->NbEntities();
-  for (Standard_Integer i = 1; i <= nb; i++) {
-
-    Handle(StepRepr_Representation) ent =
-        Handle(StepRepr_Representation)::DownCast(theModel->Value(i));
-    if (ent.IsNull()) continue;
-    if (ent->Name().IsNull()) continue;
-    std::cout << ent->Name()->ToCString() << std::endl;
-  }
-  return;
-}
-
-void sew_and_append(TopoDS_Shape shape, Handle(TopTools_HSequenceOfShape) & shape_list) {
+void sew_and_append(TopoDS_Shape shape, TopTools_HSequenceOfShape& shape_list) {
   // sew together all the curves
   BRepOffsetAPI_Sewing(1.0e-06, Standard_True);
   BRepOffsetAPI_Sewing sew;
   sew.Add(shape);
   sew.Perform();
   shape = sew.SewedShape();
-  shape_list->Append(shape);
-  return;
+  shape_list.Append(shape);
+}
+
+void sew_shapes(const TopoDS_Shape& shape, TopTools_HSequenceOfShape& shape_list) {
+  if (shape.ShapeType() == TopAbs_COMPOUND) {
+    // decend and get children
+    for (TopoDS_Iterator it = TopoDS_Iterator(shape); it.More(); it.Next()) {
+      sew_shapes(it.Value(), shape_list);
+    }
+  } else if (shape.ShapeType() == TopAbs_SOLID) {
+    // insert into the list
+    sew_and_append(shape, shape_list);
+  } else {
+    std::cout << "Unknown shape type " << shape.ShapeType() << std::endl;
+  }
 }
 
 int main(int argc, char *argv[]) {
 
-  std::string cad_file(argv[1]);
+  std::string brep_file(argv[1]);
   std::string ftol(argv[2]);
   facet_tol = std::stod(ftol);
   std::string filename(argv[3]);
 
   moab::ErrorCode rval = mbtool->set_tags();
 
-  step = new STEPControl_Reader();
-  step->ReadFile(cad_file.c_str());
-
-  step->PrintCheckLoad(false, IFSelect_ListByItem);
-  step->ClearShapes();
-  int count = step->NbRootsForTransfer();
   TopoDS_Shape shape;
+  BRep_Builder builder;
+  BRepTools::Read(shape, brep_file.c_str(), builder);
 
-  int r_count = step->TransferRoots();
-  std::cout << "r_count: " << r_count << std::endl;
-  std::cout << "n_shapes: " << step->NbShapes() << std::endl;
+  TopTools_HSequenceOfShape shape_list;
+  sew_shapes(shape, shape_list);
 
-  Handle(TopTools_HSequenceOfShape) shape_list = new TopTools_HSequenceOfShape;
-  std::cout << count << std::endl;
-  for (int i = 1; i <= count; i++) {
-    bool ok = step->TransferRoot(i);
-    step->PrintCheckTransfer(false, IFSelect_CountByItem);
-    if (ok) {
-      shape = step->Shape(i);
-      // if its a compound decend and get its children
-      if (shape.ShapeType() == 0) {
-        TopoDS_Iterator it = TopoDS_Iterator(shape);
-        while (it.More()) {
-          // if we are a volume
-          if (it.Value().ShapeType() == 2) {
-            sew_and_append(it.Value(), shape_list);
-          } else {
-            std::cout << "Unknown shape type " << it.Value().ShapeType() << std::endl;
-          }
-          it.Next();
-        }
-      } else if (shape.ShapeType() == 2) {
-        // we are a normal volume insert into
-        // the list
-        sew_and_append(shape, shape_list);
-      } else {
-        std::cout << "Unknown shape" << std::endl;
-      }
-    } else {
-      std::cout << "Couldnt read shape " << std::endl;
-    }
-  }
-  std::cout << "Instanciated " << shape_list->Length() << " items from file" << std::endl;
-
-  std::cout << count << " entities read from file " << std::endl;
-
-  //dump_labels();
+  std::cout << "Instanciated " << shape_list.Length() << " items from file" << std::endl;
 
   facet_all_volumes(shape_list);
   mbtool->write_geometry(filename.c_str());

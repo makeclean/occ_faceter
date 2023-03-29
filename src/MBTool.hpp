af26d393aeb4e8ccf6a962c2dc5c1faf0edc3b35
Sam Mason <sam@samason.uk>
2023-03-29 18:51:07 +0100

    Remove unused edge_data struct

--- src/MBTool.hpp
+++ src/MBTool.hpp
@@ -27,10 +27,6 @@ typedef std::vector<std::array<double, 3>> facet_coords;
 typedef std::vector<std::array<int, 3>> facet_connectivity;
 typedef std::map<moab::EntityHandle, moab::EntityHandle> ent_ent_map;
 
-struct edge_data {
-  std::vector<int> connectivity;
-};
-
 class MBTool {
 public:
   MBTool();
